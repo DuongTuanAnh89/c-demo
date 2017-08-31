a2dbac0883fe92bbffe392132398a123d71ac885
Duong Tuan Anh <Anhdtd00501@fpt.edu.vn>
2017-08-31 20:45:58 +0700

    bai hoc moi

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char *argv[]) {
-	return 0;
+#include<stdio.h>
+ main(){
+	
+	 printf ("%i\n", plus(6,7));
+	 printf ("%i", minus(6,7));
 }
+    //Công hai so voi tham so dau vao
+    // la a,b la hai so nguyen
+int plus(int a, int b){
+	return  a +b;
+
+}
+int minus (int a,int b){
+	return a-b;
+}
+    
+
