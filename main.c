30e729995ca14cf8eccfe6fc508413415747d3e4
Duong Tuan Anh <Anhdtd00501@fpt.edu.vn>
2017-08-31 21:23:00 +0700

    cộng trừ nhân chia

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
- main(){
+float devision (float a,float b);
+	
+  main(){
 	
 	 printf ("%i\n", plus(6,7));
-	 printf ("%i", minus(6,7));
+	 printf ("%i\n", minus(6,7));
+	 printf ("%i\n", multiply(5,8));
+	 printf ("%f",devision(20,6));
 }
     //Công hai so voi tham so dau vao
     // la a,b la hai so nguyen
@@ -13,5 +17,10 @@ int plus(int a, int b){
 int minus (int a,int b){
 	return a-b;
 }
-    
+int multiply (int a,int b){
+	return a*b;
+}
+float devision (float a,float b){
+	return (float) a/b;
+}
 
