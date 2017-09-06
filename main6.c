675bc3946f59a1a70fcec093d1f3bda385ca540a
Duong Tuan Anh <Anhdtd00501@fpt.edu.vn>
2017-09-06 23:50:23 +0700

    bài tập 05/09/2017

---
+++ main6.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+main(){
+  while("true"){
+  int a;
+      printf ("______Menu_____\n");
+      printf ("1.công hai sô\n:");
+      printf ("2.tru hai so\n:");
+      printf ("3.nhân hai sô\n:");
+      printf ("4.chia hai so\n:");
+      printf ("5. Thoát\n:");
+      printf ("===================\n");
+      printf ("so cua a la:");
+      scanf("%i",&a);
+
+  //chon menu:
+  switch(a){
+  	case 1 :
+  		printf ("nhâp hai so:");
+  	    break;
+  	case 2 :
+	    printf ("trù hai sô:");
+		break;
+	case 3 :
+	    printf ("nhân hai sô:");
+		break;
+	case 4 :
+	    printf ("chia hai so:");
+		break; 
+	case 5 :
+	    printf (" Thoát :");
+	
+}
+    printf ("ban da chon thoat:\n");
+    if (a==5){
+    	break;
+}
+}
+}
+        
+
+  		
+  	
+  	
+  	
+  	
+  	
+  	
+  	
+  	
+  	
+  	
+  	
+  	
+  
